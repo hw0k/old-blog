2e224612ae6dc9e0e57b7feb1eb43f9076ab4361
hw0k <hw0knam@gmail.com>
2020-12-28 19:31:37 +0900

    add GA id

--- gatsby-config.js
+++ gatsby-config.js
@@ -66,7 +66,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        // trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-186198985-1`,
       },
     },
     `gatsby-plugin-feed-mdx`,
