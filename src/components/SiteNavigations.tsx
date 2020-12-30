408d81fb828c63ea4024a0cc7e5c6c30da39de57
hw0k <hw0knam@gmail.com>
2020-12-30 19:57:44 +0900

    change about page to linkedin

--- src/components/SiteNavigations.tsx
+++ src/components/SiteNavigations.tsx
@@ -1,5 +1,6 @@
 import React, { useCallback } from 'react';
 import { Link } from 'gatsby';
+import { OutboundLink } from 'gatsby-plugin-google-analytics';
 
 const navigations = [
   {
@@ -7,20 +8,26 @@ const navigations = [
     name: '홈',
   },
   {
-    path: '/about',
+    path: 'https://www.linkedin.com/in/hw0k/',
     name: '소개',
+    isOutboundLink: true,
   },
 ];
 
 function SiteNavigations() {
-  const renderNavigation = useCallback(
-    ({ path, name }: typeof navigations[number]) => (
+  const renderNavigation = useCallback(({ path, name, isOutboundLink }: typeof navigations[number]) => {
+    const item = <li className="hover:text-green-100">{name}</li>;
+    const component = isOutboundLink ? (
+      <OutboundLink key={path} href={path}>
+        {item}
+      </OutboundLink>
+    ) : (
       <Link key={path} to={path} activeClassName="text-white">
-        <li className="hover:text-green-100">{name}</li>
+        {item}
       </Link>
-    ),
-    [],
-  );
+    );
+    return component;
+  }, []);
 
   return (
     <nav>
